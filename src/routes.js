38acd2c38f37849452aba98de5091004a2956615
marcosaureliodiasmoura <marcos_moura@live.com>
2020-03-26 00:41:10 -0300

    Add Config ESLint+Prettier+EditorConfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Olá mundo" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Olá mundo' });
 });
 
 module.exports = routes;
