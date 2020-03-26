fa11de457748feb435de6828a573c07e637e0b1e
marcosaureliodiasmoura <marcos_moura@live.com>
2020-03-26 11:17:26 -0300

    Models And Migration for Table Helpme

--- src/database/migrations/20200326040030-create-helpme.js
+++ src/database/migrations/20200326040030-create-helpme.js
@@ -1,23 +1,58 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('helpme', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      product: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      latitude: {
+        type: Sequelize.DECIMAL(9, 6),
+        allowNull: false,
+      },
+      longitude: {
+        type: Sequelize.DECIMAL(9, 6),
+        allowNull: false,
+      },
+      gift: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+  down: (queryInterface) => {
+    return queryInterface.dropTable('helpme');
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Olá mundo' });
+  return res.json({ message: 'Olá mundo, vamos salvar vidas' });
 });
 
 module.exports = routes;
